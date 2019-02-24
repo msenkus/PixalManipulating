c8608308fa92f70dd29fb1cd341f0a0d4a9c29e9
mark senkus <marksenkus@gmail.com>
2019-02-24 00:35:31 -0500

    GreyScale File change. renamed file as well as main method change

--- src/com/projects/GreyScale.java
+++ src/com/projects/GreyScale.java
@@ -27,6 +27,6 @@ public class GreyScale{
                 img.setRGB(i, j, x.getRGB());
             }
         }
-        ImageIO.write(img, "jpg", new File("C:\\Users/mark/Pictures\\Waterfall\\Waterfall2.jpg"));
+        ImageIO.write(img, "jpg", new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\waterfall2.jpg"));
     }
 }
--- src/com/projects/Main.java
+++ src/com/projects/Main.java
@@ -8,13 +8,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        File imgFile1 = new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\Punishersymbol1.jpg");
+        File imgFile = new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\waterfall.jpg");
+        File imgFile1 = new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\Punishersymbol.jpg");
         File imgFile2 = new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\waterfall2.jpg");
-        //GreyScale.gryScale(imgFile1);
+        GreyScale.gryScale(imgFile);
 
-        matrixValues mat = new matrixValues(imgFile1);
+//        matrixValues mat = new matrixValues(imgFile1);
        // mat.printAlphaMatrix();
-            mat.printDotMatrix();
+//            mat.printDotMatrix();
 
 //        HideImage val = new HideImage(imgFile1, imgFile2);
 //        val.EncryptImage();
