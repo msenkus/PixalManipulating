3136a0843bae3c0a039fd860928be6e02b53d81d
mark senkus <marksenkus@gmail.com>
2019-02-24 22:13:45 -0500

    Work on the hideImage class. There is a mixup in the RGB values turning the image red. Kinda interesting effect to exploregit status

--- src/com/projects/HideImage.java
+++ src/com/projects/HideImage.java
@@ -1,9 +1,13 @@
 package com.projects;
-import java.awt.image.BufferedImage;
+import java.awt.image.*;
 import java.io.*;
 import java.awt.*;
 import java.nio.Buffer;
 import javax.imageio.*;
+
+import static java.awt.image.BufferedImage.TYPE_BYTE_BINARY;
+import static java.awt.image.BufferedImage.TYPE_INT_ARGB;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
 
 public class HideImage{
@@ -12,20 +16,16 @@ public class HideImage{
     File hideFile;
     BufferedImage baseImg;
     BufferedImage hideImg;
+   // BufferedImage EncryptedImage = new BufferedImage(baseImg.getWidth(), baseImg.getHeight(), TYPE_INT_ARGB);
 
-    public HideImage(File BaseImage){
-        try {
-            this.baseFile = BaseImage;
-            this.baseImg = ImageIO.read(BaseImage);
-        }catch (Exception e){
-            System.out.print("heloo");
-        }
-    }
     //Constuctor
     public HideImage(File BaseFile, File hideFile){
         try {
             this.baseImg = ImageIO.read(BaseFile);
             this.hideImg = ImageIO.read(hideFile);
+            if(hideImg.getWidth() > baseImg.getWidth() || hideImg.getHeight() > baseImg.getHeight()){
+                this.hideImg = hideImg.getSubimage(0,0, baseImg.getWidth(),baseImg.getHeight());
+            }
             this.baseFile = BaseFile;
             this.hideFile = hideFile;
         }catch (Exception e){
@@ -55,23 +55,43 @@ public class HideImage{
     }
 
 
-    public BufferedImage EncryptImage(){
-        BufferedImage encryptedImage = null;
-        for( int i = 0; i < this.baseImg.getWidth(); i++){
-            for(int j = 0; j < this.baseImg.getHeight(); j++){
+    public void EncryptImage(){
+//        BufferedImage encryptedImage = null;
+        try {
+            for (int i = 0; i < this.baseImg.getWidth(); i++) {
+                for (int j = 0; j < this.baseImg.getHeight(); j++) {
 //             getColor(baseImg, i, j)
-               Color base = getColor(baseImg, i, j);
-                int baseRed = base.getRed();
-                int baseGreen = base.getGreen();
-                int baseBlue = base.getBlue();
-                System.out.println(baseRed);
+                    Color base = getColor(baseImg, i, j);
+                    int baseRed =  base.getRed();
+                    int baseGreen =  base.getGreen();
+                    int baseBlue =  base.getBlue();
 
+                    //hide-- is storing the RGB values of the image you want to hide.
+                    Color hide = getColor(hideImg, i, j);
+                    int hideRed = hide.getRed();
+                    int hideGreen = hide.getGreen();
+                    int hideBlue = hide.getBlue();
+
+                    // en-- is the encrypted variable
+                    int enRed = combSigNumbers(baseRed, hideRed);
+                    int enGreen = combSigNumbers(baseGreen, baseBlue);
+                    int enBlue = combSigNumbers(baseBlue, hideBlue);
+
+                    Color enRGB = new Color(enRed, enGreen, enBlue);
+                    hideImg.setRGB(i, j, enRGB.getRGB());
+                }
             }
-        }
-        return encryptedImage;
+        }catch(Exception e){}
+//        return encryptedImage;
     }
 
-
-
+    public int combSigNumbers(int num1, int num2){
+        num1 = (int) (Math.floor(num1/16));
+        num2 = (int) ((Math.floor(num2/16)) * 16);
+        return num1 + num2;
+    }
+    public void WriteEncryptImg() throws IOException{
+            ImageIO.write(hideImg, "jpg", new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\waterfall2.jpg"));
+    }
 
 }
--- src/com/projects/Main.java
+++ src/com/projects/Main.java
@@ -11,17 +11,16 @@ public class Main {
         File imgFile = new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\waterfall.jpg");
         File imgFile1 = new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\Punishersymbol.jpg");
         File imgFile2 = new File("C:\\Users\\mark\\IdeaProjects\\PixelImage\\WaterFall\\waterfall2.jpg");
-        GreyScale.gryScale(imgFile);
+//        GreyScale.gryScale(imgFile);
 
-//        matrixValues mat = new matrixValues(imgFile1);
-       // mat.printAlphaMatrix();
-//            mat.printDotMatrix();
 
-//        HideImage val = new HideImage(imgFile1, imgFile2);
-//        val.EncryptImage();
+        HideImage val = new HideImage(imgFile, imgFile);
+        val.EncryptImage();
+        val.WriteEncryptImg();
 
-//        BufferedImage img2 = ImageIO.read(imgFile);
-//        System.out.println(img2.getHeight());
-//        System.out.println(img2.getWidth());
+/*              Prints out the dot matrix of the punisher symbol file.
+        matrixValues mat = new matrixValues(imgFile1);
+        mat.printDotMatrix();
+*/
     }
 }
